2f43a35e1c2c56326cca654c4522930e97e14cb7
ritesh-khandekar <riteshkhandekar4003@gmail.com>
2022-12-13 03:14:00 +0530

    footer

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './footer.css'
+
+const Footer = () => {
+    return <>
+        <div className="footer">
+            <div className='footer-center'>
+                <a className="footer-link">About</a>
+                <a className="footer-link">Privacy policy</a>
+            </div>
+        </div>
+    </>
+}
+
+export default Footer
